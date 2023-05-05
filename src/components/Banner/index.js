8de815595ce76eaf437ddb5e7747040941b71df7
Jorge jr <jorgesilvajunior10@gmail.com>
2023-05-05 14:45:49 -0300

    add: header e rodape

---
+++ src/components/Banner/index.js
@@ -0,0 +1,1 @@
+import style from './Banner.module.css'; 
---
+++ src/pages/inicio/index.js
@@ -0,0 +1,13 @@
+import Cabecalho from "components/Cabecalho"
+import Rodape from "components/Rodape"
+
+function inicio() {
+    return (
+        <>
+            <Cabecalho></Cabecalho>
+            <Rodape></Rodape>
+        </>
+    )
+}
+
+export default inicio

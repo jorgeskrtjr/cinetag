987121f211b4fabc6ea5e7323805cc0b6b5facf3
Jorge jr <jorgesilvajunior10@gmail.com>
2023-05-08 15:39:36 -0300

    add: Cards, Titulo

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,25 @@
+import styles from './Inicio.module.css'
+
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import videos from "json/db.json"
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
             <Banner imagem="home"/>
+            <Titulo> <h1>Um lugar para guarda seus vídeos e filmes !</h1> </Titulo>
+            <section className={styles.container}>
+                {
+                    videos.map((video) =>{
+                       return <Card {...video} key={video.id}/>
+                    })
+                }
+            </section>
             <Rodape/>
         </>
     )

a7668ffb249fb3d296667c8a7607c764126239b0
Jorge jr <jorgesilvajunior10@gmail.com>
2023-05-09 16:03:43 -0300

    add: useState

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,24 @@
 import styles from './Card.module.css'
-import iconeFavoritar from './favoritar.png'
+import iconeFavoritar from './desfavoritar.png'
+import iconeDesfavoritar from './favoritar.png'
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 function Card({id, titulo, capa}){
+    
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+
+
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
     return(
          <div className={styles.container}>
              <img src={capa} alt={titulo} className={styles.capa}/>
              <h2>{titulo}</h2>
-             <img src={iconeFavoritar} alt='Favoritar filme' className={styles.favoritar}/> 
+             <img src={icone} alt='Favoritar filme' className={styles.favoritar} 
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }} /> 
          </div>
     )
 }
--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -2,17 +2,23 @@ import styles from './Favoritos.module.css'
 import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
 import Card from 'components/Card';
+import { useFavoritoContext } from 'contextos/Favoritos';
+
 
 function Favoritos() {
+
+    const { favorito } = useFavoritoContext(); 
+
     return (
         <>
             <Banner imagem="favoritos" />
             <Titulo> <h1>Aqui está os seus filmes favoritos !</h1> </Titulo>
             <section className={styles.container}>
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
+                {favorito.map((fav) => {
+                    
+                    return <Card {...fav} className={styles.item} key={fav.id} />
+                    
+                })}
             </section>
         </>
     )
